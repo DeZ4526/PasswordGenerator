fb1cb76ff4e60fc6d77f276477d7138193240e69
DeZ4526 <karpov.k-r@yandex.ru>
2022-10-22 20:40:08 +0300

    Create ModelViev

--- PasswordGenerator/PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/PasswordGenerator/MainWindow.xaml.cs
@@ -1,33 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PasswordGenerator
 {
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-
-		private void Button_GenPasw(object sender, RoutedEventArgs e)
-		{
-			MessageBox.Show(RandomText.GenRandText(true, true, true, true, 1000));
-		}
+		public MainWindow() => InitializeComponent();
 	}
 }
---
+++ PasswordGenerator/PasswordGenerator/ViewModels/PGViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PasswordGenerator.ViewModels
+{
+	class PGViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName] string prop = "")
+		=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		private bool? useCapitalLetters = true;
+		public bool? UseCapitalLetters
+		{
+			get => useCapitalLetters;
+			set
+			{
+				useCapitalLetters = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private bool? useLowerLetters = true;
+		public bool? UseLowerLetters
+		{
+			get => useLowerLetters;
+			set
+			{
+				useLowerLetters = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private bool? useNumbers = true;
+		public bool? UseNumbers
+		{
+			get => useNumbers;
+			set
+			{
+				useNumbers = value;
+				OnPropertyChanged();
+			}
+		}
+		private bool? useSpecialChars = true;
+		public bool? UseSpecialChars
+		{
+			get => useSpecialChars;
+			set
+			{
+				useSpecialChars = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private bool? useMyChars = false;
+		public bool? UseMyChars
+		{
+			get => useMyChars;
+			set
+			{
+				useMyChars = value;
+				OnPropertyChanged();
+			}
+		}
+		private string myChars = "";
+		public string MyChars
+		{
+			get => myChars;
+			set
+			{
+				myChars = value;
+				OnPropertyChanged();
+			}
+		}
+		public bool? IsSelectedUse
+		{
+			get => UseCapitalLetters == true || UseLowerLetters == true || (UseMyChars == true && MyChars != "") || UseNumbers == true || UseSpecialChars == true ;
+		}
+		private string password;
+		public string Password
+		{
+			get => password;
+			set
+			{
+				password = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private int length = 20;
+		public int Length
+		{
+			get => length;
+			set
+			{
+				length = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ICommand GeneratePassword
+		{
+			get => new DelegateCommand((obj) => Password = IsSelectedUse == true ? 
+				RandomText.GenRandText(useCapitalLetters == true, useLowerLetters == true, useNumbers == true, useSpecialChars == true, length, useMyChars == true ? myChars :"") : 
+				"");
+			
+		}
+		public ICommand CopyPassword
+		{
+			get
+			{
+				return new DelegateCommand((obj) => Clipboard.SetText(Password));
+			}
+		}
+	}
+}
